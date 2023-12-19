2ed7290cc2f86827dd9dae468d3c492a07008d72
chetan9222 <chetan.ghadge143@gmail.com>
2023-12-19 16:25:50 +0530

    type conversion function e.g float to int oct hex etc.

---
+++ Type_conversion_function.py
@@ -0,0 +1,20 @@
+#In python we convert the value from float to int
+a=30.33
+print(a)
+
+h=int(a) # this will convert a floating variable to integer
+print(h)
+
+#instead of passing number we are passing a string and print floating point
+
+i=float("22.55")
+print(i)
+print(type(i))
+
+#the bin function will give us binary conversion of any numeric value.
+#similar for hex function
+#Similar for oct
+print(bin(15))
+print(hex(255))
+print(oct(12))
+
