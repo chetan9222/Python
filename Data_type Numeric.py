e13af9d57d6e744e5cbef2a6b9c692af94692eca
chetan9222 <chetan.ghadge143@gmail.com>
2023-12-19 15:18:08 +0530

    All data types in python

--- Data_type Numeric.py
+++ Data_type Numeric.py
@@ -1,1 +1,29 @@
-this id test
+#This is interger data type
+a=10
+print(a)
+print(type(a)) #We can use type function in python to check out any variable data type
+
+#This is float data type
+b=250.50
+print(b)
+print(type(b))
+
+#This is binary (0B) data type. But python prints it 10 and its data type is Int in python.
+c=0B1010
+print(c)
+print(type(c))
+
+#This is Hexadecimal (0X) data type. But python prints it 255 and its data type is Int in python.
+d=0XFF
+print(d)
+print(type(d))
+
+#This is Complex data type. Means mathematical expression in maths term. python prints as it is 3+5j
+e=3+5j
+print(e)
+print(type(e))
+
+#This is Boolean  data type. But python prints it True
+f=True
+print(f)
+print(type(f))
