53f7875f3e79afa0bd1b7651714f9eeb1d1dbbbd
chetan9222 <chetan.ghadge143@gmail.com>
2023-12-20 15:25:57 +0530

    all numeric hexadecimal binary octal and integer conversion function

--- Type_conversion_function.py
+++ Type_conversion_function.py
@@ -18,3 +18,26 @@ print(bin(15))
 print(hex(255))
 print(oct(12))
 
+#integer to hexadecimal conversion function
+b=255
+print(hex(b))
+
+#integer to binary conversion function
+c=10
+print(bin(c))
+
+#integer to octal conversion function
+d=15
+print(oct(d))
+
+#hexadecimal to int
+e=0xfa
+print(int(e))
+
+#binary to int
+f=0b1011
+print(int(f))
+
+#Octal to int
+g=0o101
+print(int(g))
