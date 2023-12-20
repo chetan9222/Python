46253da841e69523184594cee9a2f882486e879a
chetan9222 <chetan.ghadge143@gmail.com>
2023-12-20 15:30:19 +0530

    added identifire, magic function, rules of variable name,private function theory

---
+++ identifires.py
@@ -0,0 +1,23 @@
+# The names are we give to variables are called Identifires
+#we need to follow certain rules whilw define identifires.
+#e.g UserID= 101
+#here UserID is the identifire
+#Rule 1 - Python is case sensetive. means UserID and Userid are two different variables.
+
+#Rule 2 - we can use any numbers in between variable name e.g user123ID, userID1234. but variable name should not shart with any number e.g 123userID
+
+#Rule 3 - we are also use underscore e.g User_ID you can use any number of underscore at the end of variable name e.g User_id____
+#But underscore at the beginning  of the variable name e.g _user_id have different meaning in python
+# single Underscore at the beginning e.g _userid  means the method or variable is private
+# Double Underscore at the beginning e.g __userid  means the method or variable is really private
+# Double Underscore at the beginning and ending e.g __user_id__ means that function is the special function or magic function.
+
+# we can not use any spacial character in the variable name e.g !@#$%^&*()
+# we can not use key word as a variable name. e.g if
+
+userID=123
+print(userID)
+
+userid=98
+print(userid)
+
